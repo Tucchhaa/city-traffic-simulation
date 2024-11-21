548fbeaaebdcb49d033a9293b357bc2e3c7a06dc
kwkwkwkak <jonathanlai0717@gmail.com>
2024-11-21 16:15:54 +0800

    updates as per feedbacks

--- Scripts/VehicleController.cs
+++ Scripts/VehicleController.cs
@@ -10,12 +10,11 @@ public class VehicleController : MonoBehaviour
 {
     public event System.Action HitWall;
 
-    private const float MAX_DISTANCE = 10000;
-    private const float MAX_VEL = 20f;
-    private const float ACCELERATION = 8f;
-    private const float TURN_SPEED = 100;
-    
-    bool UseUserInput = false;
+    public float MAX_DISTANCE = 10000;
+    public float MAX_VEL = 20f;
+    public float ACCELERATION = 8f;
+    public float TURN_SPEED = 100;
+    public float DEBUG_RAY_LEN = 5f;
 
     private float VEL_FRICT = 1f;
 
@@ -36,20 +35,16 @@ public class VehicleController : MonoBehaviour
 
     void Start()
     {
-        List<double> weight1 = XavierInit(5, 4);
-        List<double> weight2 = XavierInit(4, 3);
-        //weight1 = new List<double>(new double[] { 0, 0, 0, 0,
-        //                                          0, 0, 0, 0,
-        //                                          1, 0, 0, 0,
-        //                                          0, 0, 0, 0,
-        //                                          0, 0, 0, 0,});
-        //weight2 = new List<double>(new double[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } );   
+        List<double> weight1 = XavierInit(5, 4); // weights to map layer 1 to layer 2 (5 -> 4)
+        List<double> weight2 = XavierInit(4, 3); // weights to map layer 2 to layer 3 (4 -> 3)
         List<List<double>> weightslist = new List<List<double>> { weight1, weight2 };
+
+        // biases that will ne added to each non-input layer [4, 3]
         List<List<double>> biaslist = new List<List<double>> { new() { 0, 0, 0, 0 }, new() { 0, 0, 0 } };
-        fnn = new NN(new List<int> { 5, 4, 3 }, 
+        fnn = new NN(new List<int> { 5, 4, 3 }, // input layer, hidden layer(s), output layer
                      weightslist, 
                      biaslist, 
-                     new List<string> { "tanh", "tanh" });
+                     new List<string> { "tanh", "tanh" }); //activation functions used at each non-input layer (relu, sigmoid, tanh)
     }
     
     List<double> XavierInit(int NIn, int NOut)
@@ -66,14 +61,12 @@ public class VehicleController : MonoBehaviour
     }
     void Update()
     {
-        if (UseUserInput)
-            GetInput();
+        //GetUserInput();
     }
 
     void FixedUpdate ()
     {
-        if (!UseUserInput)
-            GetInput();
+        GetNNInput();
 
         ApplyInput();
 
@@ -82,50 +75,47 @@ public class VehicleController : MonoBehaviour
         ApplyFriction();
     }
 
-    private void GetInput()
+    private void GetNNInput()
     {
-        if (UseUserInput)
-        {
-            if (Input.GetKeyDown("w"))
-                gasInput = 1.0;
-            else if (Input.GetKeyUp("w"))
-                gasInput = 0.0;
-            else if (Input.GetKeyDown("a"))
-                turningInput = 1.0;
-            else if (Input.GetKeyUp("a"))
-                turningInput = 0.0;
-            else if (Input.GetKeyDown("d"))
-                turningInput = -1.0;
-            else if (Input.GetKeyUp("d"))
-                turningInput = 0.0;
-            else if (Input.GetKeyDown("s"))
-                VEL_FRICT = 20f;
-            else if (Input.GetKeyUp("s"))
-                VEL_FRICT = 1f;
-        }
-        else
-        {
-            double[] distances = new double[] { DistInDir(new Vector3(-1, 0, 0)),
+        double[] distances = new double[] { DistInDir(new Vector3(-1, 0, 0)),
                                                 DistInDir(new Vector3(-1, 0, 1)),
                                                 DistInDir(new Vector3(0, 0, 1)),
                                                 DistInDir(new Vector3(1, 0, 1)),
                                                 DistInDir(new Vector3(1, 0, 0))};
-            Debug.Log("ditances: " + string.Join(", ", distances));
-            fnn.SetInput(distances);
+        Debug.Log("ditances: " + string.Join(", ", distances));
+        fnn.SetInput(distances);
 
-            double[] outputs = fnn.ForwardPass();
-            gasInput = outputs[0];
-            turningInput = outputs[1];
-            VEL_FRICT = Mathf.Max(1f, 40 * (float)outputs[2]);
-        }
+        double[] outputs = fnn.ForwardPass();
+        gasInput = outputs[0];
+        turningInput = outputs[1];
+        VEL_FRICT = Mathf.Max(1f, 40 * (float)outputs[2]);
+    }
+    private void GetUserInput()
+    {
+        if (Input.GetKeyDown("w"))
+            gasInput = 1.0;
+        else if (Input.GetKeyUp("w"))
+            gasInput = 0.0;
+        else if (Input.GetKeyDown("a"))
+            turningInput = 1.0;
+        else if (Input.GetKeyUp("a"))
+            turningInput = 0.0;
+        else if (Input.GetKeyDown("d"))
+            turningInput = -1.0;
+        else if (Input.GetKeyUp("d"))
+            turningInput = 0.0;
+        else if (Input.GetKeyDown("s"))
+            VEL_FRICT = 20f;
+        else if (Input.GetKeyUp("s"))
+            VEL_FRICT = 1f;
     }
 
     private double DistInDir(Vector3 dir)
     {
         Vector3 worldDirection = transform.TransformDirection(dir.normalized);
 
-        Debug.DrawRay(transform.position, worldDirection * 5, UnityEngine.Color.red);
-        Ray ray = new Ray(transform.position + new Vector3(0, 0.1f, 0), worldDirection);
+        Debug.DrawRay(transform.position, worldDirection * DEBUG_RAY_LEN, UnityEngine.Color.red);
+        Ray ray = new Ray(transform.position + new Vector3(0, 0.1f, 0), worldDirection); // +0.1 in the y axis just to make sure it doesnt collide with the ground
 
         if (Physics.Raycast(ray, out RaycastHit hit))
             return hit.distance;
@@ -136,15 +126,8 @@ public class VehicleController : MonoBehaviour
 
     private void ApplyInput()
     {
-        if (gasInput > 1)
-            gasInput = 1;
-        else if (gasInput < 0)
-            gasInput = 0;
-        
-        if (turningInput > 1)
-            turningInput = 1;
-        else if (turningInput < -1)
-            turningInput = -1;
+        Math.Clamp(gasInput, 0, 1);
+        Math.Clamp(turningInput, -1, 1);
 
         bool canAccelerate = false;
         if (gasInput < 0)
