cc34a34b2631e139b921e82c00f3f2792cd2aa91
kwkwkwkak <153440219+kwkwkwkak@users.noreply.github.com>
2024-12-02 16:14:04 +0800

    added EC code (#4)
    Co-authored-by: Eldar Iusupzhanov <ferrarijed@gmail.com>

---
+++ Scripts/EvolutionManager.cs
@@ -0,0 +1,329 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Serialization;
+
+internal class Genotype
+{
+    public readonly List<double> Weights;
+    public float Fitness;
+
+    public Genotype(List<double> weights, float fitness)
+    {
+        Weights = weights;
+        Fitness = fitness;
+    }
+}
+
+internal class Vehicle
+{
+    public Genotype Genotype;
+    public GameObject GameObject;
+    public int ID;
+    public VehicleController Controller;
+    public DateTime StartTime;
+
+    public Vehicle(Genotype genotype, GameObject gameObject)
+    {
+        Genotype = genotype;
+        GameObject = gameObject;
+        ID = gameObject.GetInstanceID();
+        Controller = gameObject.GetComponent<VehicleController>();
+        StartTime = DateTime.Now;
+    }
+    
+    public float GetFitness()
+    {
+        return (float)(DateTime.Now - StartTime).TotalSeconds;
+    }
+}
+
+public class EvolutionManager : MonoBehaviour
+{
+    public int populationSize = 15;
+    public GameObject prefab;
+    public float crossoverChance = 0.5f;
+    public float mutationRate = 0.05f;
+    public float mutationAmount = 1f;
+
+    public List<int> layerSizes = new() { 5, 4, 3 };
+    // activation functions used at each non-input layer (relu, sigmoid, tanh)
+    public List<string> activationFuncList = new() { "tanh", "tanh" };
+    
+    private Transform _spawnPoint;
+    private int _aliveVehicleCount;
+    private readonly Dictionary<int, Vehicle> _vehicles = new ();
+    private readonly System.Random _rand = new ();
+
+    private List<Genotype> Genotypes
+    {
+        get
+        {
+            var result = new List<Genotype>();
+
+            foreach (var (_, vehicle) in _vehicles)
+                result.Add(vehicle.Genotype);
+
+            return result;
+        }
+
+        set
+        {
+            var index = 0;
+            
+            foreach (var (_, vehicle) in _vehicles)
+                vehicle.Genotype = value[index++];
+        }
+    }
+    
+    private void Start()
+    {
+        // Disable collision between vehicles
+        int vehicleLayer = LayerMask.NameToLayer("Vehicle");
+        Physics.IgnoreLayerCollision(vehicleLayer, vehicleLayer, true);
+
+        _spawnPoint = transform;
+        
+        var genotypes = GenerateGenotypes();
+        InstantiateVehicles(genotypes);
+        SpawnVehicles();
+    }
+
+    private List<Genotype> GenerateGenotypes()
+    {
+        var genotypes = new List<Genotype>();
+        
+        for (var i = 0; i < populationSize; i++)
+        {
+            var weights = new List<double>();
+            
+            for (var j = 0; j < layerSizes.Count - 1; j++)
+            {
+                weights.AddRange(XavierInit(layerSizes[j], layerSizes[j + 1]));
+                weights.AddRange(GenerateGaussianList(layerSizes[j + 1]));
+            }
+
+            genotypes.Add(new Genotype(weights, 0));
+        }
+
+        return genotypes;
+    }
+
+    private void InstantiateVehicles(List<Genotype> genotypes)
+    {
+        for (var i = 0; i < populationSize; i++)
+        {
+            var instance = Instantiate(prefab, _spawnPoint.position, _spawnPoint.rotation);
+            var vehicle = new Vehicle(genotypes[i], instance);
+            
+            vehicle.Controller.OnHitWall += () =>
+            {
+                DisableVehicle(vehicle);
+            };
+            _vehicles.Add(vehicle.ID, vehicle);
+        }
+    }
+    
+    // ReSharper disable Unity.PerformanceAnalysis
+    private void SpawnVehicles()
+    {
+        // allWeights is the list of weights for all vehicles
+        // for each vehicle, the weights list format is as follows
+        // [layer1->2 weights. layer2 biases, layer2->3 weights, layer3 biases]
+        // the list is flattened to 1D
+
+        _aliveVehicleCount = populationSize;
+
+        foreach ((int _, var vehicle) in _vehicles)
+        {
+            var genotype = vehicle.Genotype;
+            var instance = vehicle.GameObject;
+            
+            instance.transform.position = _spawnPoint.position;
+            instance.transform.rotation = _spawnPoint.rotation;
+
+            instance.SetActive(true);
+
+            var weights = new List<List<double>>();
+            var biasList = new List<List<double>>(); // biases that will be added to each non-input layer
+
+            var lastIndex = 0;
+
+            for (var j = 0 ; j < layerSizes.Count - 1; j++)
+            {
+                int weightsLen = layerSizes[j] * layerSizes[j + 1];
+                weights.Add(genotype.Weights.GetRange(lastIndex, weightsLen));
+
+                lastIndex += weightsLen;
+
+                biasList.Add(genotype.Weights.GetRange(lastIndex, layerSizes[j + 1]));
+
+                lastIndex += layerSizes[j + 1];
+            }
+
+            vehicle.Controller.Fnn = new NN(layerSizes, weights, biasList, activationFuncList);
+        }
+    }
+
+    private void DisableVehicle(Vehicle vehicle)
+    {
+        vehicle.Genotype.Fitness = vehicle.GetFitness();
+        vehicle.GameObject.SetActive(false);
+
+        _aliveVehicleCount -= 1;
+
+        if (_aliveVehicleCount == 0)
+        {
+            RespawnVehicles();
+        }
+    }
+
+    private void RespawnVehicles()
+    {
+        // reproduction
+        var intermediateGeneration = RemainderStochasticSampling(Genotypes);
+        var newGeneration = Recombination(intermediateGeneration);
+        
+        Genotypes = Mutate(newGeneration);
+
+        SpawnVehicles();
+    }
+
+    private List<Genotype> RemainderStochasticSampling(List<Genotype> genotypes)
+    {
+        var generation = new List<Genotype>();
+
+        float fitnessSum = genotypes.Sum(g => g.Fitness);
+
+        foreach (var g in genotypes)
+        {
+            float expectedCount = g.Fitness / fitnessSum * populationSize;
+
+            var copies = (int)expectedCount;
+            float extraCopyChance = expectedCount - copies;
+
+            for (var i = 0; i < copies; i++)
+                generation.Add(g);
+
+            if (_rand.NextDouble() < extraCopyChance)
+                generation.Add(g);
+        }
+
+        return generation;
+    }
+
+    private List<Genotype> Recombination(List<Genotype> intermediateGeneration)
+    {
+        var newGeneration = new List<Genotype>();
+        intermediateGeneration.Sort((a, b) => b.Fitness.CompareTo(a.Fitness));
+
+        Debug.Log("Best Fitness in population: " + intermediateGeneration[0].Fitness);
+        // keep best 2
+        newGeneration.Add(intermediateGeneration[0]);
+        newGeneration.Add(intermediateGeneration[1]);
+
+        while (newGeneration.Count < populationSize)
+        {
+            int ind1 = _rand.Next(0, intermediateGeneration.Count), ind2 = _rand.Next(0, intermediateGeneration.Count);
+                
+            while (ind1 == ind2)
+                ind2 = _rand.Next(0, intermediateGeneration.Count);
+
+
+            Genotype offspring1, offspring2;
+            (offspring1, offspring2) = CompleteCrossover(intermediateGeneration[ind1], intermediateGeneration[ind2]);
+
+            newGeneration.Add(offspring1);
+
+            if (newGeneration.Count < populationSize)
+                newGeneration.Add(offspring2);
+        }
+
+        return newGeneration;
+    }
+
+    private (Genotype, Genotype) CompleteCrossover(Genotype parent1, Genotype parent2)
+    {
+        List<double> weights1 = new List<double>(),
+                     weights2 = new List<double>();
+
+        int weightsCount = parent1.Weights.Count;
+
+        if (parent2.Weights.Count != weightsCount)
+            Debug.LogError("Crossover parents have different sizes");
+
+        for (var i = 0; i < weightsCount; i++)
+        {
+            if (_rand.NextDouble() < crossoverChance)
+            {
+                weights1.Add(parent2.Weights[i]);
+                weights2.Add(parent1.Weights[i]);
+            }
+            else
+            {
+                weights1.Add(parent1.Weights[i]);
+                weights2.Add(parent2.Weights[i]);
+            }
+        }
+        
+        return (new Genotype(weights1, 0), new Genotype(weights2, 0));
+    }
+
+    private List<Genotype> Mutate(List<Genotype> generation)
+    {
+        for (var i = 0; i < generation.Count; i++)
+        {
+            int weightsCount = generation[i].Weights.Count;
+            var weights = generation[i].Weights;
+            
+            for (var j = 0; j < weightsCount; j++)
+            {
+                if (_rand.NextDouble() < mutationRate)
+                {
+                    weights[j] += _rand.NextDouble() * mutationAmount * 2 - mutationAmount;
+                }
+            }
+
+            generation[i] = new Genotype(weights, 0);
+        }
+
+        return generation;
+    }
+
+    private List<double> XavierInit(int NIn, int NOut)
+    {
+        var weights = new List<double>();
+        int size = NIn * NOut;
+
+        for (var i = 0; i < size; i++)
+        {
+            // Uniform random in range [-sqrt(6/(n_in+n_out)), sqrt(6/(n_in+n_out))]
+            weights.Add(Math.Sqrt(6.0 / (NIn + NOut)) * (_rand.NextDouble() * 2.0 - 1.0));
+        }
+
+        return weights;
+    }
+
+    private List<double> GenerateGaussianList(int size, double mean = 0.0, double stddev = 1.0)
+    {
+        var gaussianNumbers = new List<double>();
+
+        for (var i = 0; i < size; i++)
+        {
+            gaussianNumbers.Add(RandomGaussian(mean, stddev));
+        }
+
+        return gaussianNumbers;
+    }
+
+    private double RandomGaussian(double mean = 0.0, double stddev = 1.0)
+    {
+        // Use the Box-Muller transform
+        double u1 = 1.0 - _rand.NextDouble(); // Uniform(0,1] random number
+        double u2 = 1.0 - _rand.NextDouble();
+        double z = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
+        return z * stddev + mean;
+    }
+}
--- Scripts/VehicleController.cs
+++ Scripts/VehicleController.cs
@@ -4,63 +4,40 @@ using System;
 
 public class VehicleController : MonoBehaviour
 {
-    public event System.Action HitWall;
-
-    private const float MaxDistance = 10000;
-    private const float DebugRayLen = 5f;
+    public event Action OnHitWall;
+    
+    private const float MaxRaycastDistance = 10000;
 
     public float maxVelocity = 20f;
     public float accelerationFactor = 8f;
     public float turnFactor = 100;
     public float frictionFactor = 40f;
-    public Vector3 rayBias = new (0, 0.1f, 0);
+    public Vector3 rayBias = new (0, 0.5f, 0);
+    public LayerMask rayMask = ~0;
 
-    private NN _fnn;
+    public NN Fnn;
     private float _velocity;
     private Quaternion _rotation;
+    private Collider _collider;
 
     private void Start()
     {
-        var layerSizes = new List<int> { 5, 4, 3 };
-        var weightsList = new List<List<double>>
-        {
-            XavierInit(5, 4), // weights to map layer 1 to layer 2 (5 -> 4)
-            XavierInit(4, 3)  // weights to map layer 2 to layer 3 (4 -> 3)
-        };
-        
-        // biases that will be added to each non-input layer [4, 3]
-        var biasList = new List<List<double>>
-        {
-            new() { 0, 0, 0, 0 }, 
-            new() { 0, 0, 0 }
-        };
-        
-        // activation functions used at each non-input layer (relu, sigmoid, tanh)
-        var activationFuncList = new List<string> { "tanh", "tanh" };
-        
-        _fnn = new NN(layerSizes, weightsList, biasList, activationFuncList); 
+        _collider = GetComponent<Collider>();
     }
     
-    private List<double> XavierInit(int NIn, int NOut)
-    {
-        System.Random rand = new System.Random();
-        List<double> weights = new List<double>();
-        int size = NIn * NOut;
-        
-        for (int i = 0; i < size ; i++)
-        {
-            // Uniform random in range [-sqrt(6/(n_in+n_out)), sqrt(6/(n_in+n_out))]
-            weights.Add(Math.Sqrt(6.0 / (NIn + NOut)) * (rand.NextDouble() * 2.0 - 1.0));
-        }
-
-        return weights;
-    }
-    
-    private void FixedUpdate ()
+    private void FixedUpdate()
     {
         var (gas, turning, friction) = GetNnOutput();
-
+        
         MoveVehicle(gas, turning, friction);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Ground")
+            return;
+        
+        OnHitWall?.Invoke();
     }
 
     private (float, float, float) GetNnOutput()
@@ -73,10 +50,10 @@ public class VehicleController : MonoBehaviour
             GetDirectionDistance(new Vector3(1, 0, 0))
         };
         
-        // Debug.Log("ditances: " + string.Join(", ", distances));
-        _fnn.SetInput(distances);
+        //Debug.Log("ditances: " + string.Join(", ", distances));
+        Fnn.SetInput(distances);
 
-        var outputs = _fnn.ForwardPass();
+        var outputs = Fnn.ForwardPass();
         var gas  = (float)outputs[0];
         var turn = (float)outputs[1];
         var friction = Mathf.Max(1f, frictionFactor * (float)outputs[2]);
@@ -84,33 +61,43 @@ public class VehicleController : MonoBehaviour
         return (gas, turn, friction);
     }
 
-    private double GetDirectionDistance(Vector3 dir)
+    private double GetDirectionDistance(Vector3 direction)
     {
-        Vector3 worldDirection = transform.TransformDirection(dir.normalized);
+        var worldDirection = transform.TransformDirection(direction.normalized);
 
-        Debug.DrawRay(transform.position, worldDirection * DebugRayLen, Color.red);
+        // Disable own collider, so raycast wouldn't hit it
+        _collider.enabled = false;
         
         // Add ray bias to make sure ray doesn't hit the ground
-        Ray ray = new Ray(transform.position + rayBias, worldDirection);
+        var rayPoint = transform.position + rayBias;
+        var ray = new Ray(rayPoint, worldDirection);
 
-        if (Physics.Raycast(ray, out RaycastHit hit))
-            return hit.distance;
+        float distance = Physics.Raycast(ray, out var hit, MaxRaycastDistance, rayMask)
+            ? hit.distance
+            : MaxRaycastDistance;
         
-        return MaxDistance;
+        Debug.DrawRay(rayPoint, worldDirection * distance, Color.green);
+
+        _collider.enabled = true;
+
+        return distance;
     }
 
     private void MoveVehicle(float gas, float turn, float friction)
     {
         Math.Clamp(gas, 0, 1);
         Math.Clamp(turn, -1, 1);
-        
+
         if (gas == 0) {
             _velocity = Mathf.MoveTowards(_velocity, 0, friction * Time.deltaTime);
         }
         else
         {
             _velocity += gas * accelerationFactor * Time.deltaTime;
-            _velocity = Mathf.Clamp(_velocity, -maxVelocity, maxVelocity);
+            // _velocity = Mathf.Clamp(_velocity, -maxVelocity, maxVelocity);
+
+            // needs further adjustments
+            _velocity = Mathf.Clamp(_velocity, 0.5f * maxVelocity, maxVelocity);
         }
 
         _rotation = transform.rotation;
